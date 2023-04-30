188a1d0d5c0826cd39b63748bb4a2c7cb35f9a86
Jongok Yoon <yoonjongok1209@gmail.com>
2023-04-30 11:33:38 +0200

    darkmode toggle switch done

--- src/components/DarkModeSwitch/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch/DarkModeSwitch.tsx
@@ -11,22 +11,18 @@ interface DarkModeSwitchProps {
 export const DarkModeSwitch: React.FC<DarkModeSwitchProps> = ({ isDarkMode, toggleTheme }) => {
   return (
     <Switch
-      checked={isDarkMode}
+      checked={!isDarkMode}
       onChange={toggleTheme}
       className={clsx(
-        'cursur-pointer relative inline-flex px-[2px] items-center h-[22px] w-[42px] shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75',
-        {
-          'bg-accent': isDarkMode,
-          'bg-input': !isDarkMode,
-        }
+        'cursur-pointer relative inline-flex px-[2px] items-center h-[24px] w-[48px] shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 bg-grey-5'
       )}
     >
       <span
-        className={clsx(
-          'cursur-pointer inline-block h-[14px] w-[14px] transform rounded-full bg-blue-600 shadow-lg ring-0 transition duration-200 ease-in-out',
+        className={cn(
+          'cursur-pointer inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out',
           {
-            'translate-x-5': isDarkMode,
-            'translate-x-0': !isDarkMode,
+            'translate-x-[25px]': !isDarkMode,
+            'translate-x-[1px]': isDarkMode,
           }
         )}
       />
--- src/components/ui/Drawer/Drawer.tsx
+++ src/components/ui/Drawer/Drawer.tsx
@@ -1,5 +1,11 @@
 import { Dialog } from '@headlessui/react';
-import { Button } from '../Button/Button';
+import { FlexBoxColumn } from '../FlexBoxColumn/FlexBoxColumn';
+import { ReactComponent as Logo } from '../../../assets/icons/logo.svg';
+import { ReactComponent as MoonIcon } from '../../../assets/icons/icon-dark-mode.svg';
+import { ReactComponent as SunIcon } from '../../../assets/icons/icon-light-mode.svg';
+import { FlexBoxRow } from '../FlexBoxRow/FlexBoxRow';
+import { DarkModeSwitch } from '../../DarkModeSwitch/DarkModeSwitch';
+import { useState } from 'react';
 
 type DrawerProps = {
   title: string;
@@ -9,32 +15,39 @@ type DrawerProps = {
 };
 
 export default function Drawer({ title, children, isOpen, setIsOpen }: DrawerProps) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <Dialog
       unmount={false}
       open={isOpen}
       onClose={() => setIsOpen(false)}
-      className='fixed w-full max-w-[250px] z-30 inset-0 overflow-y-auto bg-black-3'
+      className='fixed w-full max-w-[250px] flex flex-col gap-[27px] px-6 py-[28px] z-30 inset-0 overflow-y-auto bg-black-3'
     >
+      <Logo />
+
       <div className='flex h-screen'>
         <Dialog.Overlay className='z-40 fixed inset-0 bg-black bg-opacity-30' />
-        <div
-          className={`z-50 flex flex-col justify-between  w-full
-                         max-w-sm p-6 overflow-hidden text-left align-middle
+        <FlexBoxColumn
+          fullWidth
+          className={`z-50 justify-between
+                         max-w-sm  overflow-hidden text-left align-middle
                          shadow-xl`}
         >
-          <div>
+          <FlexBoxColumn fullWidth className='gap-[26px]'>
             <Dialog.Title className='font-bold md:text-4xl text-grey-4 typography'>
               {title}
             </Dialog.Title>
             {children}
-          </div>
-          <div className='self-center mt-10'>
-            <Button buttonTitle='newDocument' onClick={() => setIsOpen(!isOpen)}>
-              Close
-            </Button>
-          </div>
-        </div>
+          </FlexBoxColumn>
+          <FlexBoxRow fullWidth intent={'flexStartCenter'} className='gap-[10px]'>
+            <MoonIcon fill={isDarkMode ? 'white' : '#5A6069'} />
+            <DarkModeSwitch
+              isDarkMode={isDarkMode}
+              toggleTheme={() => setIsDarkMode((prev) => !prev)}
+            />
+            <SunIcon fill={!isDarkMode ? 'white' : '#5A6069'} />
+          </FlexBoxRow>
+        </FlexBoxColumn>
       </div>
     </Dialog>
   );
